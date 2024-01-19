7fdd58dbe259d219b8daf0327841ca03661adc4a
Shayla Beltran <sbeltran@uchicago.edu>
2024-01-19 13:32:40 -0600

    first half of loom 1/19

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,46 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum meep; 
+      
     //variables
-    public float runSpeed;
-    public float jumpSpeed;
-    public float dashSpeed;
-    public float dashDuration;
-    bool isDashing;
-    public LayerMask groundMask;
-    private Rigidbody2D rigidBody;
-    private BoxCollider2D boxCollider;
+    public float RunSpeed;
+    public float JumpSpeed;
+    public float DashSpeed;
+    public float DashDuration;
+    bool IsDashing;
 
+    public LayerMask groundMask;
+    private Rigidbody2D _rigidBody;
+    private BoxCollider2D _boxCollider;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     { 
-        rigidBody = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDashing)
+        if (IsDashing)
         {
             return;
         }
         float horizontalInputs = Input.GetAxis("Horizontal");
 
-        rigidBody.velocity = new Vector2(runSpeed * horizontalInputs, rigidBody.velocity.y);
+        _rigidBody.velocity = new Vector2(RunSpeed * horizontalInputs, _rigidBody.velocity.y);
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, JumpSpeed);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             StartCoroutine(Dash());
         }
+
+        UpdateAnimation(horizontalInputs);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -59,16 +65,42 @@ public class Player : MonoBehaviour
 
     bool IsGrounded()
     {
-        bool isGrounded = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundMask);
+        bool isGrounded = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundMask);
         return isGrounded;
     }
 
     IEnumerator Dash()
     { 
-        isDashing = true;
+        IsDashing = true;
         float horizontalInputs = Input.GetAxis("Horizontal");
-        rigidBody.velocity = new Vector2(dashSpeed * horizontalInputs, rigidBody.velocity.y);
-        yield return new WaitForSeconds(dashDuration);
-        isDashing = false;
+        _rigidBody.velocity = new Vector2(DashSpeed * horizontalInputs, _rigidBody.velocity.y);
+        yield return new WaitForSeconds(DashDuration);
+        IsDashing = false;
+    }
+
+    private void UpdateAnimation(float horizontalInput)
+    {
+        bool isRunning;
+
+        if (horizontalInput > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+        if (horizontalInput != 0)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+
+        }
+
+        _animator.SetBool("isRunning", isRunning);
     }
 }
